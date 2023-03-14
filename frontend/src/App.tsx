3fada18f6f3b283f684d01c01f0e83324b78412c
lorde78 <basil.lombard@hetic.net>
2023-03-14 10:38:46 +0100

    adding form formik and yup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,20 @@
+// @ts-ignore 
 import React from "react";
+// @ts-ignore 
 import logo from "./logo.svg";
+// @ts-ignore 
+import MyForm from "./components/Form";
 import "./App.css";
 
 
 function App() {
 	return (
 		<div className="App">
-			<head>
-				<link href="/dist/output.css" rel="stylesheet"></link>
-			</head>
-			<header className="App-header">
-				<img src={logo} className="App-logo h-6" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				{/* <a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a> */}
-				<div className="flex h-6 text-3xl font-bold underline">
-					<p>HELLO</p>
-					<p>REGISTER</p>
-				</div>
-			</header>
+
+			<MyForm></MyForm>
+
+
+
 		</div>
 	);
 }
