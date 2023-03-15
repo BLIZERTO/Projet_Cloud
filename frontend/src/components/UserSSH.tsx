943528ec43d2ab3f679d71227d28c5a94771cd89
lorde78 <basil.lombard@hetic.net>
2023-03-15 16:26:51 +0100

    alloooooooooo

--- frontend/src/components/UserSSH.tsx
+++ frontend/src/components/UserSSH.tsx
@@ -2,11 +2,17 @@
 import React, { useState, useEffect } from "react";
 // @ts-ignore
 import axios from "axios";
+// @ts-ignore
+import jwt_decode from "jwt-decode";
 
 interface PersonalInfoProps {
 	username: string;
 	sshpassword: string;
 }
+
+// interface TokenPayload {
+// 	token: any;
+// }
 
 const PersonalInfo: React.FC<PersonalInfoProps> = ({ username, sshpassword }) => {
 	return (
@@ -17,23 +23,36 @@ const PersonalInfo: React.FC<PersonalInfoProps> = ({ username, sshpassword }) =>
 	);
 };
 
+function getToken(token: string): any {
+	const tokenString = localStorage.getItem(token) as string;
+
+	const userToken = jwt_decode(tokenString);
+	return userToken;
+}
+
+
+
 const CollapsiblePersonalInfo: React.FC = () => {
+
+	const decodedToken = getToken("token");
+	console.log(decodedToken.id);
 	const [isCollapsed, setIsCollapsed] = useState<boolean>(true);
 	const [personalInfo, setPersonalInfo] = useState<PersonalInfoProps>({
 		username: "",
 		sshpassword: "",
 	});
+	
 
 	const handleButtonClick = () => {
 		setIsCollapsed(!isCollapsed);
 	};
 
 	useEffect(() => {
-		axios.get("http://localhost:3000/api/getallvolumesbyid").then((response) => {
+		axios.post("http://localhost:4000/api/getallvolumesbyid", {data : {id: decodedToken.id}}).then((response) => {
 			setPersonalInfo(response.data);
-			console.log(response)
 			console.log(response.data)
 		});
+		console.log(getToken)
 	}, []);
 
 	return (
--- frontend/src/pages/Archives.tsx
+++ frontend/src/pages/Archives.tsx
@@ -1,11 +1,10 @@
 // @ts-ignore 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 // @ts-ignore 
 import Footer from "../components/Footer";
 // @ts-ignore 
 import CollapsiblePersonalInfo from '../components/UserSSH';
-//@ts-ignore
-import axios from 'axios';
+
 
 
 
