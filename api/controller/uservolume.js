6ea7aa057e5d0392817238b0c35feced81a38b70
ouzepe <je.ouzepe@gmail.com>
2023-03-15 16:23:05 +0100

    volume

--- api/controller/uservolume.js
+++ api/controller/uservolume.js
@@ -1,13 +1,12 @@
-const {UserVolume} = require("../model/index");
+const {UserVolume, User} = require("../model/index");
 
 const getAllUserVolumeByID = async (req, res) => {
 // need volume id not user id
-  let volume = await UserVolume.find({_id: req.body.id}).populate({path: "creatorID", select: "-password"}).select("email").select("name").select("username").select("ssh_password")
+  let volume = await UserVolume.find({creatorID: req.body.id}).populate({path: "creatorID", select: "-password"}).select("email").select("name").select("username").select("ssh_password")
   res.json({volume: volume})
 };
 
 const getAllVolume = async (req, res) => {
-    // need volume id not user id
       let volume = await UserVolume.find().populate({path: "creatorID", select: "-password"}).select("email").select("name").select("username").select("ssh_password")
       res.json({volume: volume})
     };
