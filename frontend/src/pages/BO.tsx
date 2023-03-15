33ed3e32e78af5d24372a74acd43a9255eaf4868
MadShiro <rayanatrouni@gmail.com>
2023-03-15 11:33:58 +0100

    page bo

--- frontend/src/pages/BO.tsx
+++ frontend/src/pages/BO.tsx
@@ -12,10 +12,10 @@ const BO = () => {
             <div className='flex justify-start content'>
                 <h1><b>Accueil</b></h1>
             </div>
-            <div>
-                
+            <div className='flew row presentation'>
+                Aliquam scelerisque elit quam, sit amet euismod ipsum semper in. Vivamus sodales sollicitudin dapibus. Fusce id tellus quis leo feugiat pellentesque nec sed justo. Suspendisse tristique, nibh at tincidunt efficitur, ipsum mi blandit diam, eu bibendum orci ex sed felis. Donec faucibus luctus tortor, at lacinia justo tristique a. Morbi dapibus vestibulum nunc a convallis. 
+                In bibendum ullamcorper odio, ut tempus elit eleifend eget. Fusce euismod lorem non tincidunt sagittis.
             </div>
-               
         </div>
         
     );
