72e4aab19eea8e0ee3297afeeb9d1255e66c648b
MadShiro <rayanatrouni@gmail.com>
2023-03-16 10:01:31 +0100

    page bo terminé + avatar

--- frontend/src/pages/BO.tsx
+++ frontend/src/pages/BO.tsx
@@ -5,21 +5,31 @@ import { displayPartsToString } from 'typescript';
 import Navigation from '../components/Navigation';
 // import MyForm from "../components/Form";
 import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
 
 const BO = () => {
     return (
         <div>
             <Navigation></Navigation>
+            <div className='avatar'>
+                <img src="./assets/Ellipse_2avatar.png" alt="Logo" />
+            </div>
             <div>
                 <h1 className='titre'><b>Accueil</b></h1>
             </div>
             <div className='flex flex-row presentation'>
-                <div className='flex flex-col'>
+                <div className='flex flex-col content'>
                 Aliquam scelerisque elit quam, sit amet euismod ipsum semper in. Vivamus sodales sollicitudin dapibus. Fusce id tellus quis leo feugiat pellentesque nec sed justo. Suspendisse tristique, nibh at tincidunt efficitur, ipsum mi blandit diam, eu bibendum orci ex sed felis. Donec faucibus luctus tortor, at lacinia justo tristique a. Morbi dapibus vestibulum nunc a convallis. 
                 In bibendum ullamcorper odio, ut tempus elit eleifend eget. Fusce euismod lorem non tincidunt sagittis.
                </div> 
             </div>
-            <Footer></Footer>
+            <div className='buttons'>
+                <Link to="/about">Compte utilisateur</Link>
+                <Link to="/about">Ajouter un projet</Link>
+            </div>
+            <footer>
+                <Footer></Footer>
+            </footer>  
         </div>
         
     );
