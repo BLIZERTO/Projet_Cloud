7bf3849225e050f175d7cd0b7ddeaf8bb5315500
Pierre Magnien <magniendcpierre@gmail.com>
2023-03-17 16:38:15 +0100

    clear result for db size

--- api/controller/getDBStatsFromSsh.js
+++ api/controller/getDBStatsFromSsh.js
@@ -4,7 +4,9 @@ const {getSshDBStats} = require('../ssh/getSizeDatas')
  const getDBStatsFromSsh =  async (req, res) => {
      try {
          const stats = await getSshDBStats(req.body.name);
-         res.status(200).json(stats);
+         const regex = /\b(db|collections|objects|avgObjSize|dataSize|storageSize|numExtents|indexes|indexSize|ok)\b/g;
+         const result = stats.replace(regex, '');
+         res.status(200).json(result);
      } catch (error) {
          console.error(error);
          res.status(500).json('Internal server error');
