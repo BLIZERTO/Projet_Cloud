e3811b9b9bb19c1aa5be911ba1c9e825d71b4293
Pierre Magnien <magniendcpierre@gmail.com>
2023-03-17 16:39:00 +0100

    reverse

--- api/controller/getDBStatsFromSsh.js
+++ api/controller/getDBStatsFromSsh.js
@@ -4,9 +4,7 @@ const {getSshDBStats} = require('../ssh/getSizeDatas')
  const getDBStatsFromSsh =  async (req, res) => {
      try {
          const stats = await getSshDBStats(req.body.name);
-         const regex = /\b(db|collections|objects|avgObjSize|dataSize|storageSize|numExtents|indexes|indexSize|ok)\b/g;
-         const result = stats.replace(regex, '');
-         res.status(200).json(result);
+         res.status(200).json(stats);
      } catch (error) {
          console.error(error);
          res.status(500).json('Internal server error');
