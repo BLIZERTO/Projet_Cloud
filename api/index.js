0a902990808222b19269b352b6c3110eda11c962
ouzepe <je.ouzepe@gmail.com>
2023-03-17 15:59:25 +0100

    log

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const winston = require("winston");
 const app = express();
 const routes = require("./routes");
 const dotenv = require('dotenv');
@@ -20,10 +21,26 @@ app.use(express.static('Public'));
 ///initial routes
 app.use("/api", routes);
 
+///////// Logging ///////////
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.File({
+      filename: 'myapp.log',
+      level: 'info',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+      )
+    })
+  ]
+});
+
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.url} ${JSON.stringify(req.body)}`);
+  next();
+});
 
 /////////start server//////////////
-app.listen(process.env.PORT || 5000, () => console.log(`server run at ${`http://localhost:${process.env.PORT}/api`}`));
-
-
-
-
+app.listen(process.env.PORT || 5000, () => {
+  logger.info(`server run at ${`http://localhost:${process.env.PORT}/api`}`);
+});
