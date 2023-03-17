e729d5207c5395bc07a14d30a46363282a45cb4d
Pierre Magnien <magniendcpierre@gmail.com>
2023-03-17 12:03:06 +0100

    fix createproject

--- api/ssh/sshCMD/createproject.sh
+++ api/ssh/sshCMD/createproject.sh
@@ -16,7 +16,15 @@
   echo ">>> Access for $projectname to Write & Read given to $username"
 
   # Création de la configuration du projet
-  sudo sh "./nginx-config.sh" "$projectname" "$username"
+
+WEB_DIR='/home/'$2
+
+sed -e "s,_;,'$1';,g;s,root_dir,'$WEB_DIR/$1'," /etc/nginx/sites-enabled/default > /etc/nginx/sites-enabled/$1
+
+echo -e "\n#Added by nginx-server-block-generator.sh\n127.0.0.1     $1" >> /etc/hosts
+
+/etc/init.d/nginx restart
+
   echo ">>> Project Config created"
 
 echo "Disconnected from server"
